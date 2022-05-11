bb1d8a039f5d6918794270535cd773c9b38eda2b
bellakez <bellakezia28@stud.kea.dk>
2022-05-11 17:18:09 +0200

    start doc

--- cwc_page.php
+++ cwc_page.php
@@ -11,7 +11,28 @@
 
 get_header();
 ?>
+<html lang="da">
+	<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!--Selv-defineret meta beskrivelse-->
+    <meta name="description" content="Coach" />
+    <meta name="keywords" content="coach, stress, ledelse, HTML, CSS, JavaScript" />
+    <meta name="author" content="Nina og Isabella" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<!--fonte-->
 
+    <!--faveicon-->
+    <!--link til stylesheet-->
+	<link rel="stylesheet" href="style.css">
+	<title>Charlotte Wahl</title>
+<!--_____________________________________________________________________________________-->
 
+    <body>
+        
+        
+    </body>
+</html>
 <?php
 get_footer();
